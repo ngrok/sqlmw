3f95e0f0133186768a0df7b55cb62ae8d4791c04
ohayn <oy.iagan.777@gmail.com>
2020-01-29 15:53:13 +0900

    Add tests for stmt's CheckNamedValue

---
+++ fakedb_test.go
@@ -0,0 +1,84 @@
+package sqlmw
+
+import (
+	"context"
+	"database/sql/driver"
+)
+
+type fakeDriver struct {
+	conn driver.Conn
+}
+
+func (d *fakeDriver) Open(_ string) (driver.Conn, error) {
+	return d.conn, nil
+}
+
+type fakeStmt struct {
+	called bool
+}
+
+type fakeStmtWithCheckNamedValue struct {
+	fakeStmt
+}
+
+type fakeStmtWithoutCheckNamedValue struct {
+	fakeStmt
+}
+
+func (s fakeStmt) Close() error {
+	return nil
+}
+
+func (s fakeStmt) NumInput() int {
+	return 1
+}
+
+func (s fakeStmt) Exec(_ []driver.Value) (driver.Result, error) {
+	return nil, nil
+}
+
+func (s fakeStmt) Query(_ []driver.Value) (driver.Rows, error) {
+	return nil, nil
+}
+
+func (s *fakeStmtWithCheckNamedValue) CheckNamedValue(_ *driver.NamedValue) (err error) {
+	s.called = true
+	return
+}
+
+type fakeConn struct {
+	called bool
+	stmt   driver.Stmt
+}
+
+type fakeConnWithCheckNamedValue struct {
+	fakeConn
+}
+
+type fakeConnWithoutCheckNamedValue struct {
+	fakeConn
+}
+
+func (c *fakeConn) Prepare(_ string) (driver.Stmt, error) {
+	return nil, nil
+}
+
+func (c *fakeConn) PrepareContext(_ context.Context, _ string) (driver.Stmt, error) {
+	return c.stmt, nil
+}
+
+func (c *fakeConn) Close() error              { return nil }
+func (c *fakeConn) Begin() (driver.Tx, error) { return nil, nil }
+
+func (c *fakeConn) QueryContext(_ context.Context, _ string, _ []driver.NamedValue) (driver.Rows, error) {
+	return nil, nil
+}
+
+func (c *fakeConnWithCheckNamedValue) CheckNamedValue(_ *driver.NamedValue) (err error) {
+	c.called = true
+	return
+}
+
+type fakeInterceptor struct {
+	NullInterceptor
+}
---
+++ stmt_go19_test.go
@@ -0,0 +1,106 @@
+package sqlmw
+
+import (
+	"database/sql"
+	"reflect"
+	"testing"
+)
+
+func TestWrappedStmt_CheckNamedValue(t *testing.T) {
+	tests := map[string]struct {
+		fd       *fakeDriver
+		expected struct {
+			cc bool // Whether the fakeConn's CheckNamedValue was called
+			sc bool // Whether the fakeStmt's CheckNamedValue was called
+		}
+	}{
+		"When both conn and stmt implement CheckNamedValue": {
+			fd: &fakeDriver{
+				conn: &fakeConnWithCheckNamedValue{
+					fakeConn: fakeConn{
+						stmt: &fakeStmtWithCheckNamedValue{},
+					},
+				},
+			},
+			expected: struct {
+				cc bool
+				sc bool
+			}{cc: false, sc: true},
+		},
+		"When only conn implements CheckNamedValue": {
+			fd: &fakeDriver{
+				conn: &fakeConnWithCheckNamedValue{
+					fakeConn: fakeConn{
+						stmt: &fakeStmtWithoutCheckNamedValue{},
+					},
+				},
+			},
+			expected: struct {
+				cc bool
+				sc bool
+			}{cc: true, sc: false},
+		},
+		"When only stmt implements CheckNamedValue": {
+			fd: &fakeDriver{
+				conn: &fakeConnWithoutCheckNamedValue{
+					fakeConn: fakeConn{
+						stmt: &fakeStmtWithCheckNamedValue{},
+					},
+				},
+			},
+			expected: struct {
+				cc bool
+				sc bool
+			}{cc: false, sc: true},
+		},
+		"When both stmt do not implement CheckNamedValue": {
+			fd: &fakeDriver{
+				conn: &fakeConnWithoutCheckNamedValue{
+					fakeConn: fakeConn{
+						stmt: &fakeStmtWithoutCheckNamedValue{},
+					},
+				},
+			},
+			expected: struct {
+				cc bool
+				sc bool
+			}{cc: false, sc: false},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			sql.Register("fake-driver:"+name, Driver(test.fd, &fakeInterceptor{}))
+			db, err := sql.Open("fake-driver:"+name, "dummy")
+			if err != nil {
+				t.Errorf("Failed to open: %v", err)
+			}
+			defer func() {
+				if err := db.Close(); err != nil {
+					t.Errorf("Failed to close db: %v", err)
+				}
+			}()
+
+			stmt, err := db.Prepare("SELECT foo FROM bar Where 1 = ?")
+			if err != nil {
+				t.Errorf("Failed to prepare: %v", err)
+			}
+
+			if _, err := stmt.Query(1); err != nil {
+				t.Errorf("Failed to query: %v", err)
+			}
+
+			conn := reflect.ValueOf(test.fd.conn).Elem()
+			sc := conn.FieldByName("stmt").Elem().Elem().FieldByName("called").Bool()
+			cc := conn.FieldByName("called").Bool()
+
+			if test.expected.sc != sc {
+				t.Errorf("sc mismatch.\n got: %#v\nwant: %#v", sc, test.expected.sc)
+			}
+
+			if test.expected.cc != cc {
+				t.Errorf("cc mismatch.\n got: %#v\nwant: %#v", cc, test.expected.cc)
+			}
+		})
+	}
+}
